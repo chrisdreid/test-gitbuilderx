c01323643bd118521bd9881858472750d01c41fd
chrisdreid <chris.d.reid@gmail.com>
2025-07-28 13:33:07 -0700

    updated the story with a lepricon and rocket

--- c.py
+++ c.py
@@ -4,8 +4,9 @@ INIT = "comment on line 1 and added this to line 3"
 
 TEXT_TO_MODIFY = '''
 Here is some text to midify.
-Once there lived a little frog.
-That frog ran to the store.
-... well almost got there before he got killed.
-Now we end the story here.
+Once there lived a little lepricon.
+That lepricon ran to the moon.
+-> the rocket crashed and he never was seen again.
+
+THE END.
 '''
