b57fad704c0e48006fc8e1d4031f0508372ed74c
chrisdreid <chris.d.reid@gmail.com>
2025-07-28 13:27:35 -0700

    added line 2,5,8,12

--- b.py
+++ b.py
@@ -1,5 +1,12 @@
 # this is file b.py
+# adding a comment under line 1
 
 INIT = 'init statement for file b'
+adding = 'new variable on line 5'
 
 # comment for file b.py
+'''here is a comment for line 8'''
+
+
+
+LINE_12 = 'new line 12'
