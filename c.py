bfb7f13df5573f1975b7b8598847685e649c85c4
chrisdreid <chris.d.reid@gmail.com>
2025-07-28 13:31:14 -0700

    added a story to modify in c.py

--- c.py
+++ c.py
@@ -1,3 +1,11 @@
 # c.py added
 
 INIT = "comment on line 1 and added this to line 3"
+
+TEXT_TO_MODIFY = '''
+Here is some text to midify.
+Once there lived a little frog.
+That frog ran to the store.
+... well almost got there before he got killed.
+Now we end the story here.
+'''
