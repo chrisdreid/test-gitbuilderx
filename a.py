1b219f1828b5c4f0b2d9239af581d76cd02257f9
chrisdreid <chris.d.reid@gmail.com>
2025-07-28 13:29:11 -0700

    added c.py and updated a.py

--- a.py
+++ a.py
@@ -3,3 +3,6 @@
 INIT = 'this is the init statement'
 
 # comment for a.py
+
+
+# we are going to add on line 8 a new file called c.py
---
+++ c.py
@@ -0,0 +1,3 @@
+# c.py added
+
+INIT = "comment on line 1 and added this to line 3"
